24beb125eebb5f8d40e4e7f8fb83ca0b899318b8
Panagiotis Georgakopoulos <panagiotis.georgakopoulos@juliacomputing.com>
2022-01-08 23:35:04 +0200

    Fix typo

--- bin/install.sh
+++ bin/install.sh
@@ -11,6 +11,6 @@ mv ~/.julia ~/.julia-bk
 # Recreates ~/.julia with defaults
 /tmp/julia/bin/julia -e 'using Pkg; Pkg.resolve()'
 rm -rf  ~/.julia/artifacts
-cp -R ../js_sysimg/artifacts ~/.julia/
-cp -R ../js_sysimg/PinnedProject/* ~/.julia/environments/v1.6/
-cp -R ../js_sysimg/SrclessPackages/* /tmp/julia/share/julia/stdlib/v1.6/
+cp -R ./js_sysimg/artifacts ~/.julia/
+cp -R ./js_sysimg/PinnedProject/* ~/.julia/environments/v1.6/
+cp -R ./js_sysimg/SrclessPackages/* /tmp/julia/share/julia/stdlib/v1.6/
