94ab535fd6790510307183b93188f0d2a5ade0a7
Panagiotis Georgakopoulos <panagiotis.georgakopoulos@juliacomputing.com>
2022-01-09 00:46:29 +0200

    JH runs from inside .julia :(

--- bin/install.sh
+++ bin/install.sh
@@ -9,7 +9,7 @@ rm -rf /tmp/julia
 cp -r /opt/julia-1.6.1/ /tmp/julia/
 chown -R $USER:$USER /tmp/julia
 rm -rf ~/.julia-bk
-mv ~/.julia ~/.julia-bk
+# mv ~/.julia ~/.julia-bk
 
 # Recreates ~/.julia with defaults
 /tmp/julia/bin/julia -e 'using Pkg; Pkg.resolve()'
