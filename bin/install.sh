1c7599a4bdf3de7bcfe61af90fa28101dae7ce56
Panagiotis Georgakopoulos <panagiotis.georgakopoulos@juliacomputing.com>
2022-01-09 01:17:37 +0200

    more cleanup

--- bin/install.sh
+++ bin/install.sh
@@ -7,11 +7,11 @@ set -ex
 rm -rf /tmp/julia
 cp -r /opt/julia-1.6.1/ /tmp/julia/
 chown -R $USER:$USER /tmp/julia
-rm -rf ~/.julia-bk
+# rm -rf ~/.julia-bk
 # mv ~/.julia ~/.julia-bk
 
 # Recreates ~/.julia with defaults
-/tmp/julia/bin/julia -e 'using Pkg; Pkg.resolve()'
+# /tmp/julia/bin/julia -e 'using Pkg; Pkg.resolve()'
 rm -rf  ~/.julia/artifacts
 cp -r js_sysimg/artifacts ~/.julia/
 cp -r js_sysimg/PinnedProject/* ~/.julia/environments/v1.6/
